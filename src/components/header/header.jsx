869e2eb9be03d73bc7f9c2b39ce0685bdeae8550
Silvia_Phungky <silvia@tinkerlust.com>
2020-09-30 15:00:30 +0700

    create header component & styling

---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <h4 className='header text-center py-5 font-weight-bold'>
+    Visualization of People Data
+  </h4>
+)
+
+export default Header
